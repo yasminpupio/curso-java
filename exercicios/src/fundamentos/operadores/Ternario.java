03187b42dd71e31f4e90128fc031591a2a521165
Yasmin Pupio <yasminpupio52@gmail.com>
2025-02-27 08:55:09 -0300

    Operador Ternário

---
+++ exercicios/src/fundamentos/operadores/Ternario.java
@@ -0,0 +1,21 @@
+package fundamentos.operadores;
+
+public class Ternario {
+	
+	public static void main(String[] args) {
+		
+		double media = 5.6;
+		String resultadoParcial = media > 5.0 ? "em recuperação" : "reprovado";
+		String resultadoFinal = media >= 7.0 ? "aprovado" : resultadoParcial;
+		
+		System.out.println("O aluno está " + resultadoFinal);
+		
+		double nota = 9.9;
+		boolean bomComportamento = false; 
+		boolean passouPorMedia = nota >= 7;
+		boolean temDesconto = bomComportamento && passouPorMedia;
+		String resultado = temDesconto ? "Sim" : "Não";
+		
+		System.out.printf("Tem desconto? %s", resultado);
+	}
+}
