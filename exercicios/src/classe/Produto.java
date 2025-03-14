bc2e9636678b6b7ca5aa004db1575249b645ae3f
Yasmin Pupio <yasminpupio52@gmail.com>
2025-03-14 15:03:15 -0300

    Classe Produto
    Adicionando construtores

--- exercicios/src/classe/Produto.java
+++ exercicios/src/classe/Produto.java
@@ -5,7 +5,16 @@ public class Produto {
 	String nome;
 	double preco;
 	double desconto;
-	
+
+	Produto() {
+	}
+
+	Produto(String nomeInicial, double precoInicial, double descontoInicial) {
+		nome = nomeInicial;
+		preco = precoInicial;
+		desconto = descontoInicial;
+	}
+
 	double precoComDesconto() {
 		return preco * (1 - desconto);
 	}
