be629fbc81c4d2cebb658cc1728d768f1fd35693
Yasmin Pupio <yasminpupio52@gmail.com>
2025-03-16 17:57:47 -0300

    Membro de Classe vc Instância
    Área da Circunferência

---
+++ exercicios/src/classe/AreaCircTeste.java
@@ -0,0 +1,22 @@
+package classe;
+
+public class AreaCircTeste {
+
+	public static void main(String[] args) {
+		
+		AreaCirc a1 = new AreaCirc(10);
+//		a1.pi = 10;
+		
+		AreaCirc a2 = new AreaCirc(5);
+//		a2.pi = 5;
+		
+//		AreaCirc.pi = 3.1415;
+		
+		System.out.println(a1.area());
+		System.out.println(a2.area());
+		System.out.println(AreaCirc.area(100));
+		
+		System.out.println(AreaCirc.PI);
+		System.out.println(Math.PI);
+	}
+}
