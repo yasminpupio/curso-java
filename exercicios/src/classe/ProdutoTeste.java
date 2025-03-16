d2fb81a56b13d742efd9e90300169bd188facb35
Yasmin Pupio <yasminpupio52@gmail.com>
2025-03-16 18:04:26 -0300

    Alteração no desconto do produto
    Membro de classe vs Estância

--- exercicios/src/classe/ProdutoTeste.java
+++ exercicios/src/classe/ProdutoTeste.java
@@ -4,14 +4,16 @@ public class ProdutoTeste {
 
 	public static void main(String[] args) {
 
-		Produto p1 = new Produto("Notebook", 4356.89, 0.25);
+		Produto p1 = new Produto("Notebook", 4356.89);
 
 		var p2 = new Produto();
 		p2.nome = "Caneta Preta";
 		p2.preco = 12.56;
-		p2.desconto = 0.29;
+		
+		Produto.desconto = 0.29;
 
-		System.out.println(p2.nome);
+		System.out.println(p1.nome + " " + p1.precoComDesconto());
+		System.out.println(p2.nome + " " + p2.precoComDesconto());
 		System.out.println(p1.nome);
 
 		double precoFinal1 = p1.precoComDesconto();
