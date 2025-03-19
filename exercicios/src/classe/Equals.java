fa70cc1d5c93024b27341cf8ac94bc6d588db8d6
Yasmin Pupio <yasminpupio52@gmail.com>
2025-03-19 10:24:37 -0300

    Equals

---
+++ exercicios/src/classe/Equals.java
@@ -0,0 +1,23 @@
+package classe;
+
+import java.util.Date;
+
+public class Equals {
+
+	public static void main(String[] args) {
+
+		Usuario u1 = new Usuario();
+		u1.nome = "Yasmin";
+		u1.email = "yasmin@email.com.br";
+
+		Usuario u2 = new Usuario();
+		u2.nome = "Yasmin";
+		u2.email = "yasmin@email.com.br";
+
+		System.out.println(u1 == u2);
+		System.out.println(u1.equals(u2));
+		System.out.println(u2.equals(u1));
+
+		System.out.println(u2.equals(new Date()));
+	}
+}
