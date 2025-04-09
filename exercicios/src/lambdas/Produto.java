17bd6e6df13ca138f10b0780c52c85eda4bd8f1d
Yasmin Pupio <yasminpupio52@gmail.com>
2025-04-09 17:26:00 -0300

    Consumer
    Recebe 1 parâmetro e não retorna nada. Apenas escreve no console

--- exercicios/src/lambdas/Produto.java
+++ exercicios/src/lambdas/Produto.java
@@ -11,4 +11,10 @@ public class Produto {
 		this.preco = preco;
 		this.desconto = desconto;
 	}
+	
+	@Override
+	public String toString() {
+		double precoFinal = preco * (1 - desconto);
+		return nome + " tem preço de R$" + precoFinal;
+	}
 }
