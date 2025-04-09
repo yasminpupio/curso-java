abbcc59a1be6fd57ece69c3a1ee6ddc88018492a
Yasmin Pupio <yasminpupio52@gmail.com>
2025-04-09 10:34:43 -0300

    Interfaces Funcionais #02

--- exercicios/src/lambdas/Calculo.java
+++ exercicios/src/lambdas/Calculo.java
@@ -4,4 +4,12 @@ package lambdas;
 public interface Calculo {
 
 	double executar(double a, double b);
+
+	default String legal() {
+		return "legal";
+	}
+	
+	static String muitoLegal() {
+		return "muito legal";
+	}
 }
