1d05345a28f2dd9b0c5395de9aadd622cc680965
Yasmin Pupio <yasminpupio52@gmail.com>
2025-04-09 11:27:05 -0300

    Foreach Funcional #02

--- exercicios/src/lambdas/Foreach.java
+++ exercicios/src/lambdas/Foreach.java
@@ -17,7 +17,17 @@ public class Foreach {
 		System.out.println("\nLambda #01...");
 		aprovados.forEach(nome -> System.out.println(nome + "!!!"));
 		
-		System.out.println("\nMethod Reference...");
+		System.out.println("\nMethod Reference #01...");
 		aprovados.forEach(System.out::println);
+		
+		System.out.println("\nLambda #02...");
+		aprovados.forEach(nome -> meuImprimir(nome));
+		
+		System.out.println("\nMethod Reference #02...");
+		aprovados.forEach(Foreach::meuImprimir);
+	}
+	
+	static void meuImprimir(String nome) {
+		System.out.println("Oi! Meu nome é " + nome);
 	}
 }
